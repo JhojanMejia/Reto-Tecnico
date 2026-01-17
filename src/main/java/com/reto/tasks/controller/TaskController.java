a2ebedeb51740c3eaf37d1a6146863a03e4e7aa0
JhojanMejia <mejiajhojan78@gmail.com>
2026-01-17 17:27:42 -0500

    Envio Final

--- src/main/java/com/reto/tasks/controller/TaskController.java
+++ src/main/java/com/reto/tasks/controller/TaskController.java
@@ -1,4 +1,51 @@
 package com.reto.tasks.controller;
 
+import com.reto.tasks.dto.TaskRequestDTO;
+import com.reto.tasks.dto.TaskResponseDTO;
+import com.reto.tasks.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskRequestDTO request) {
+
+        TaskResponseDTO response = taskService.createTask(request);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<java.util.List<TaskResponseDTO>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable Long id, @Valid @RequestBody TaskRequestDTO request) {
+        return ResponseEntity.ok(taskService.updateTask(id, request));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> getTaskById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build(); // Retorna 204 No Content
+    }
+    @GetMapping("/stats")
+    public ResponseEntity<java.util.Map<String, Long>> getStats() {
+        return ResponseEntity.ok(taskService.getTaskStatistics());
+    }
 }
