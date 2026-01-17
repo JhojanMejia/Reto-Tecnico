59537deb0cbb948df80d4bb4d48ce65efd5c10c4
JhojanMejia <mejiajhojan78@gmail.com>
2026-01-17 10:32:46 -0500

    Entidades y repositorios agregados

---
+++ src/main/java/com/reto/tasks/model/Status.java
@@ -0,0 +1,5 @@
+package com.reto.tasks.model;
+
+public enum Status {
+    TODO, IN_PROGRESS, DONE
+}
---
+++ src/main/java/com/reto/tasks/model/Task.java
@@ -0,0 +1,40 @@
+package com.reto.tasks.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data // Esto genera Getters, Setters, toString, etc., automáticamente
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 80)
+    private String title;
+
+    @Column(length = 250)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    private LocalDate dueDate;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
